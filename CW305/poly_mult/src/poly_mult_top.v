4657ac71076101796be7058e5fae6f8d94fd3e8a
my-rooftop <dgwogh@dgist.ac.kr>
2025-02-25 22:08:46 +0900

    [Fix] poly_mult compile complete

--- CW305/poly_mult/src/poly_mult_top.v
+++ CW305/poly_mult/src/poly_mult_top.v
@@ -40,11 +40,33 @@ module poly_mult_top #(
     reg [LOG_WEIGHT-1:0] loc_addr_read;
 
     reg [W_BY_X-1:0] din;
+    wire [ADDR_WIDTH-1:0]addr_0;
+    wire [ADDR_WIDTH-1:0]addr_1;
     reg [ADDR_WIDTH-1:0] addr_0_reg;
+    reg [ADDR_WIDTH-1:0] addr_1_reg;
     wire [W_BY_X-1:0] dout_0; // ✅ RANDOM_BITS_MEM에서 읽은 데이터
+    wire [W_BY_X-1:0] dout_1; // ✅ RANDOM_BITS_MEM에서 읽은 데이터
+    reg [ADDR_WIDTH-1:0]addr_result;
+    wire [W_BY_X-1:0]dout;
+    wire valid;
+    reg rd_dout;
 
     reg wr_en_pos;
     reg wr_en_dual;
+
+    wire [LOG_WEIGHT-1:0] loc_addr;
+    wire [ADDR_WIDTH-1:0] addr_0_mux;
+    wire [ADDR_WIDTH-1:0] addr_1_mux;
+
+    assign addr_0_mux = (load_i) ? addr_0_reg : addr_result;
+    assign addr_1_mux = (load_i) ? addr_1_reg : addr_result;
+    assign loc_addr = (load_i) ? loc_addr_write : loc_addr_read;
+
+    assign mux_word_0 = (addr_0_reg> RAMSIZE/2 - 1)? 0: dout_0;
+    assign mux_word_1 = (addr_1_reg> RAMSIZE/2 - 1)? 0: dout_1;  
+
+    assign addr_0 = addr_0_reg;
+    assign addr_1 = addr_1_reg;
 
     mem_dual #(.WIDTH(LOGW), .DEPTH(WEIGHT)) POSITION_RAM (
         .clock(clk),
@@ -64,12 +86,48 @@ module poly_mult_top #(
         .data_0(din),
         .data_1(0),
         .address_0(addr_0_reg),
-        .address_1(0),
+        .address_1(addr_1_reg),
         .wren_0(wr_en_dual),
         .wren_1(0),
         .q_0(dout_0), // ✅ 읽기 데이터
-        .q_1()
+        .q_1(dout_1)
     );
+
+    poly_mult #(
+        .MAX_WEIGHT(MAX_WEIGHT),
+        .N(N),
+        .M(M),
+        .W(W),
+        .RAMWIDTH(RAMWIDTH),
+        .X(X)
+    )
+    DUT  (
+        .clk(clk),
+        .rst(rst),
+        .start(start),
+                
+        // Shift Position loading
+        .loc_addr(loc_addr),
+        .loc_in(loc_in),
+        .weight(WEIGHT),
+        
+        // Random Vector Loading
+        .mux_word_0(mux_word_0),
+        .mux_word_1(mux_word_1),
+        .addr_0(addr_0_mux),
+		.addr_1(addr_1_mux),
+		
+		.valid(valid),
+		.addr_result(addr_result),
+		.rd_dout(rd_dout),
+		.dout(dout),
+		
+		.add_wr_en(0),
+		.add_addr(0),
+		.add_in(0)
+  );
+
+
 
     always @(posedge clk) begin
         if (load_i) begin
