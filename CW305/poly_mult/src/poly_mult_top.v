b4eaf9be9ec0759d537bdb5923b2b504fadb4e0a
my-rooftop <dgwogh@dgist.ac.kr>
2025-02-26 22:44:25 +0900

    [Fix] polymult debug check point

--- CW305/poly_mult/src/poly_mult_top.v
+++ CW305/poly_mult/src/poly_mult_top.v
@@ -45,6 +45,8 @@ module poly_mult_top #(
     wire [ADDR_WIDTH-1:0]addr_1;
     reg [ADDR_WIDTH-1:0] addr_0_reg;
     reg [ADDR_WIDTH-1:0] addr_1_reg;
+    wire [W_BY_X-1:0] mux_word_0;
+    wire [W_BY_X-1:0] mux_word_1;
     wire [W_BY_X-1:0] dout_0; // ✅ RANDOM_BITS_MEM에서 읽은 데이터
     wire [W_BY_X-1:0] dout_1; // ✅ RANDOM_BITS_MEM에서 읽은 데이터
     reg [ADDR_WIDTH-1:0]addr_result;
@@ -59,15 +61,12 @@ module poly_mult_top #(
     wire [ADDR_WIDTH-1:0] addr_0_mux;
     wire [ADDR_WIDTH-1:0] addr_1_mux;
 
-    assign addr_0_mux = (load_i) ? addr_0_reg : (start ? addr_0_reg : addr_result);
-    assign addr_1_mux = (load_i) ? addr_1_reg : (start ? addr_1_reg : addr_result);
-    assign loc_addr = (load_i) ? loc_addr_read : (start ? loc_addr_read : loc_addr_write);
+    // assign addr_0_mux = (start) ? 0 : addr_0_reg;
+    // assign addr_1_mux = (start) ? 0: addr_1_reg;
+    assign loc_addr = (start) ? 0 : loc_addr_read;
 
     assign mux_word_0 = (addr_0_reg> RAMSIZE/2 - 1)? 0: dout_0;
     assign mux_word_1 = (addr_1_reg> RAMSIZE/2 - 1)? 0: dout_1;  
-
-    assign addr_0 = addr_0_reg;
-    assign addr_1 = addr_1_reg;
 
     mem_dual #(.WIDTH(LOGW), .DEPTH(WEIGHT)) POSITION_RAM (
         .clock(clk),
@@ -82,7 +81,7 @@ module poly_mult_top #(
     );
 
     // ✅ RANDOM_BITS_MEM (32비트 저장 및 읽기)
-    mem_dual #(.WIDTH(W_BY_X), .DEPTH(Y)) RANDOM_BITS_MEM (
+    mem_dual #(.WIDTH(W_BY_X), .DEPTH(RAMSIZE)) RANDOM_BITS_MEM (
         .clock(clk),
         .data_0(din),
         .data_1(0),
@@ -115,8 +114,8 @@ module poly_mult_top #(
         // Random Vector Loading
         .mux_word_0(mux_word_0),
         .mux_word_1(mux_word_1),
-        .addr_0(addr_0_mux),
-		.addr_1(addr_1_mux),
+        .addr_0(addr_0),
+		.addr_1(addr_1),
 		
 		.valid(valid),
 		.addr_result(addr_result),
@@ -126,63 +125,89 @@ module poly_mult_top #(
 		.add_wr_en(0),
 		.add_addr(0),
 		.add_in(0)
-  );
+    );
+
+    always @(posedge clk) begin
+        if (key_i >= WEIGHT && key_i < (WEIGHT + 553)) begin
+            addr_0_reg <= key_i - WEIGHT;
+            addr_1_reg <= 0;
+        end
+        else begin
+            addr_0_reg <= addr_0; 
+            addr_1_reg <= addr_1; 
+        end
+    end
 
 
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            // ✅ 리셋이 들어오면 모든 상태 초기화
+            busy_o <= 0;
+            start <= 0;
+            wr_en_pos <= 0;
+            wr_en_dual <= 0;
+            rd_dout <= 0;
 
-        always @(posedge clk) begin
-        if (load_i) begin
+            loc_addr_write <= 0;
+            loc_addr_read <= 0;
+            data_write <= 0;
+            // addr_0_reg <= 0;
+            addr_1_reg <= 0;
+            din <= 0;
+            addr_result <= 0;
+            data_o <= 0;
+        end
+        else if (load_i) begin
             busy_o <= 1;
 
-            if (data_i == 128'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) begin // ✅ 128비트가 전부 1이면 start 신호 활성화
+            if (data_i == 128'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) begin
                 start <= 1;
                 rd_dout <= 0;
             end
-            else if (start) begin
-                start <= 0;
-            end
-            //여기 문제 가능성 큼
-            if (data_i != 0) begin // ✅ `data_i`가 0이 아니면 저장
-                if (key_i < WEIGHT) begin  // ✅ key_i < 66 → POSITION_RAM에 16비트 저장
+
+            if (data_i != 0) begin 
+                if (key_i < WEIGHT) begin
                     wr_en_pos <= 1;
                     wr_en_dual <= 0;
                     loc_addr_write <= key_i;
-                    data_write   <= data_i[15:0];  // ✅ 16비트 저장
-                    data_o <= data_i;  // ✅ 즉시 data_o 업데이트
+                    data_write   <= data_i[15:0];
+                    data_o <= data_i;
                 end
-                else if (key_i >= WEIGHT && key_i < (WEIGHT + 553)) begin  // ✅ key_i 66~(66+553) → RANDOM_BITS_MEM에 32비트 저장
+                else if (key_i >= WEIGHT && key_i < (WEIGHT + 553)) begin
                     wr_en_pos <= 0;
                     wr_en_dual <= 1;
-                    addr_0_reg <= key_i - WEIGHT;
-                    din <= data_i[31:0];  // ✅ 32비트 저장
-                    data_o <= data_i;  // ✅ 즉시 data_o 업데이트
+                    // addr_0_reg <= key_i - WEIGHT;
+                    din <= data_i[31:0];
+                    data_o <= data_i;
                 end
                 else begin
                     wr_en_pos <= 0;
                     wr_en_dual <= 0;
                 end
             end
-            else begin // ✅ `data_i == 0`이면 key_i에 해당하는 메모리 값 읽기
+            else begin
                 wr_en_pos <= 0;
                 wr_en_dual <= 0;
 
                 if (key_i < WEIGHT) begin
-                    loc_addr_read <= key_i; // ✅ POSITION_RAM에서 데이터 읽기
-                    data_o <= { 112'b0, loc_in};  // ✅ 데이터는 상위 16비트에 배치, 나머지는 0
+                    loc_addr_read <= key_i;
+                    data_o <= {112'b0, loc_in};
                 end
                 else if (key_i >= WEIGHT && key_i < (WEIGHT + 553)) begin
-                    addr_0_reg <= key_i - WEIGHT; // ✅ RANDOM_BITS_MEM에서 데이터 읽기
-                    data_o <= {96'b0, dout_0};  // ✅ 데이터는 상위 32비트에 배치, 나머지는 0
+                    // addr_0_reg <= key_i - WEIGHT;
+                    data_o <= {96'b0, dout_0};
                 end
                 else begin
-                    data_o <= 128'b1;  // ✅ 유효하지 않은 key 값일 때
+                    data_o <= 128'b1;
                 end
             end
         end
+        else if (start) begin
+            start <= 0;
+        end
         else if (start == 0 && valid) begin
-            // ✅ poly_mult 종료 후 valid가 1이 될 때까지 대기
-            busy_o <= 0; 
-            data_o <= dout;  
+            busy_o <= 0;
+            data_o <= dout;
         end 
         else begin
             busy_o <= 0;
--- CW305/poly_mult/src/tb/tb.v
+++ CW305/poly_mult/src/tb/tb.v
@@ -112,14 +112,6 @@ module tb();
          wait_done();
          read_bytes(0, 16, `REG_CRYPT_CIPHEROUT, read_data);
 
-         if (read_data == expected_cipher) begin
-               $display("Good result");
-         end
-         else begin
-               errors = errors + 1;
-               $display("ERROR: expected %h", expected_cipher);
-               $display("            got %h", read_data);
-         end
       end
    endtask
 
@@ -144,14 +136,6 @@ module tb();
          wait_done();
          read_bytes(0, 16, `REG_CRYPT_CIPHEROUT, read_data);
 
-         if (read_data == expected_cipher) begin
-               $display("Good result");
-         end
-         else begin
-               errors = errors + 1;
-               $display("ERROR: expected %h", expected_cipher);
-               $display("            got %h", read_data);
-         end
       end
    endtask
 
@@ -915,69 +899,6 @@ module tb();
          $display("ERROR: expected %h", expected_cipher);
          $display("            got %h", read_data);
       end
-
-      // $display("Encrypting via usb_trigger (USB clock disabled)...");
-      // write_bytes(0, 1, `REG_CRYPT_TEXTIN, 8'h01);
-      // expected_cipher = 128'h0efee0bff4cf170752994fb45bd45934;
-      // usb_clk_enable = 1'b0;
-      // @(posedge usb_clk) usb_trigger = 1'b1;
-      // repeat (10) @(posedge usb_clk); 
-      // usb_trigger = 1'b0;
-      // repeat (30) @(posedge pll_clk1);
-      // usb_clk_enable = 1'b1;
-      // repeat (5) @(posedge usb_clk);
-      // wait_done();
-      // read_bytes(0, 16, `REG_CRYPT_CIPHEROUT, read_data);
-      // if (read_data == expected_cipher) begin
-      //    $display("Good result");
-      // end
-      // else begin
-      //    errors = errors + 1;
-      //    $display("ERROR: expected %h", expected_cipher);
-      //    $display("            got %h", read_data);
-      // end
-
-
-      // $display("Encrypting via usb_trigger (USB clock enabled)...");
-      // write_bytes(0, 1, `REG_CRYPT_TEXTIN, 8'h02);
-      // expected_cipher = 128'h8623e205b50bede46f795d1aad15faae;
-      // @(posedge usb_clk) usb_trigger = 1'b1;
-      // repeat (10) @(posedge usb_clk); 
-      // usb_trigger = 1'b0;
-      // repeat (30) @(posedge pll_clk1);
-      // repeat (5) @(posedge usb_clk);
-      // wait_done();
-      // read_bytes(0, 16, `REG_CRYPT_CIPHEROUT, read_data);
-      // if (read_data == expected_cipher) begin
-      //    $display("Good result");
-      // end
-      // else begin
-      //    errors = errors + 1;
-      //    $display("ERROR: expected %h", expected_cipher);
-      //    $display("            got %h", read_data);
-      // end
-
-      // $display("Encrypting via usb_trigger (USB clock disabled, returns mid-encryption)...");
-      // write_bytes(0, 1, `REG_CRYPT_TEXTIN, 8'h03);
-      // expected_cipher = 128'h46be87df4d18bfe6d0d1d3b20b6bf382;
-      // usb_clk_enable = 1'b0;
-      // @(posedge usb_clk) usb_trigger = 1'b1;
-      // repeat (2) @(posedge usb_clk); 
-      // usb_trigger = 1'b0;
-      // repeat (2) @(posedge pll_clk1);
-      // usb_clk_enable = 1'b1;
-      // repeat (1) @(posedge usb_clk);
-      // wait_done();
-      // read_bytes(0, 16, `REG_CRYPT_CIPHEROUT, read_data);
-      // if (read_data == expected_cipher) begin
-      //    $display("Good result");
-      // end
-      // else begin
-      //    errors = errors + 1;
-      //    $display("ERROR: expected %h", expected_cipher);
-      //    $display("            got %h", read_data);
-      // end
-
 
 
       $display("done!");
